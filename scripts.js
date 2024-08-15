e8ce1f8da3d87bdafd86bc3f95949bdf0ad329ac
Isadora Farache <farachedev@gmail.com>
2024-08-15 18:22:53 -0300

    add-scripts

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = []
+
+
+function adicionarNovaTarefa(){
+    minhaListaDeItens.push(input.value)
+
+    input.value = ''
+
+    mostrarTarefas()
+}
+
+function mostrarTarefas(){
+    let novaLi = ''
+
+    minhaListaDeItens.forEach((tarefa) => {
+        novaLi = novaLi + `
+        
+         <li class="task">
+               <img src="./img/checked.png" alt="check-na-tarefa">
+              <p>${tarefa}</p>
+              <img src="./img/trash.png" alt="tarefa-para-o-lixo">
+         </li>
+
+
+        `
+
+    })
+
+    listaCompleta.innerHTML = novaLi
+}
+
+button.addEventListener('click', adicionarNovaTarefa)
